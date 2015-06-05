LiquidSmooth-Old/android_packages_apps_Settings

Add setting to launch BitSyko Layers App from Settings
Credits to: @ProjectDISCO
Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>

Also includes commit:
Switch to vector drawable for layers icon
ecdb859e83e30471d3ddf6de9fd3b1a1336899b3

Change-Id: I4bfcb297ca990b167c83156d7eebbd3e3117db91

src/com/android/settings/SettingsActivity.java
@@ -1209,6 +1209,16 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
+                } else if (id == R.id.bitsyko_layers) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("org.bitsyko.overlaymanager", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
@@ -1369,3 +1379,4 @@ public class SettingsActivity extends Activity
         mResultIntentData = resultIntentData;
     }
 }
+

SOKP/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -1236,6 +1236,16 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
+                } else if (id == R.id.bitsyko_layers) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.lovejoy777.rroandlayersmanager", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
@@ -1396,3 +1406,4 @@ public class SettingsActivity extends Activity
         mResultIntentData = resultIntentData;
     }
 }
+