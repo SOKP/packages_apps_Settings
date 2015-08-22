c34562dd7c48a71ff11747b2594510018374cf0b
AudioGod <AudioGod@Sonic-Developers.com>
2015-08-22 15:48:27 +0530

    Fix TRDS Tile
    Signed-off-by: AudioGod <AudioGod@Sonic-Developers.com>

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -192,6 +192,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_systemui_restart";
                 stringId = R.string.qs_systemui_restart_tile;
                 break;
+			case QSConstants.TILE_TRDS:
+                resourceName = "ic_qs_trds_on";
+                stringId = R.string.qs_tile_trds;
+                break;	
             default:
                 return null;
         }
